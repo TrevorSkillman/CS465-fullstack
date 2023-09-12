bc9a6c9bc7e119a2da6434cfca013716081029e8
Trevor Skillman <trevor.skillman@snhu.edu>
2023-09-12 18:17:23 -0400

    travel template from static data to dynamic JSON

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,10 +1,14 @@
 
 // Importing and intializing
 const packageInfo = require('../../package.json');
+// fs stands for file system that allows you to access the local file system on the browser
+const fs = require('fs');
+// the pars takes the string giving back an object.
+const trips = JSON.parse(fs.readFileSync('./data/trips.json', "utf8"));
 
 const travel = (req, res) => {
     pageTitle = packageInfo.description + ' - Travel';
-    res.render('travel', {title: pageTitle});
+    res.render('travel', {title: pageTitle, trips});
 };
 
 module.exports = {
