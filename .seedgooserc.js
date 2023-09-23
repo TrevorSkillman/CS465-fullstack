ad95a9f8edcc2d341b5e1c0d130e1042a0915d36
Trevor Skillman <trevor.skillman@snhu.edu>
2023-09-23 08:16:39 -0400

    Added in Resource Link for Solution to connect to localhost

--- .seedgooserc.js
+++ .seedgooserc.js
@@ -2,6 +2,6 @@ module.exports = {
     modelBaseDirectory: 'app_server/database/models', // model directory name
     models: ['*.js', "!db.js"], // model matcher
     data: 'data', // data directory name
-    db: 'mongodb://127.0.0.1:27017/travlr', // db connection url
+    db: 'mongodb://127.0.0.1:27017/travlr', // Due to using version 18 or higher of Node.js version. https://mongoosejs.com/docs/connections.html
   };
   
--- app_server/database/db.js
+++ app_server/database/db.js
@@ -1,72 +1,3 @@
-/*const mongoose = require('mongoose');
-const host = process.env.DB_HOST || '127.0.0.1'
-let dbURI = `mongodb://${host}/travlr`;
-const readline = require('readline');
-
-// avoid current server discovery and monitoring engine is deprecated
-mongoose.set('useUnifiedTopology', true);
-
-const connect = () => {
-    setTimeout(() => mongoose.connect(dbURI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    }), 1000);
-}
-
-mongoose.connection.on('connected', () => {
-    console.log('connected');
-});
-
-mongoose.connection.on('error', err => {
-    console.log('error: ' + err);
-    return connect();
-});
-
-mongoose.connection.on('disconnected', () => {
-    console.log('disconnected');
-});
-
-if(process.platform === 'win32') {
-    const rl = readline.createInterface({
-        input: process.stdin,
-        output: process.stdout
-    });
-    rl.on('SIGINT', () => {
-        process.emit("SIGINT");
-    });
-};
-
-const gracefulShutdown = (msg, callback) => {
-    mongoose.connection.close(() => {
-        console.log(`Mongoose disconnected through ${msg}`);
-        callback();
-    });
-};
-
-// For nodemon restarts
-process.once('SIGUSR2', () => {
-    gracefulShutdown('nodemon restart', () => {
-        process.kill(process.pid, 'SIGUSR2');
-    });
-});
-// for app termination
-process.on('SIGINT', () => {
-    gracefulShutdown('app termination', () => {
-        process.exit(0);
-    });
-});
-// For Heroku app termination
-process.on('SIGTERM', () => {
-    gracefulShutdown('Heroku app shutdown', () => {
-        process.exit(0);
-    });
-});
-
-connect();
-
-//bring in the mongoose schema
-require('./models/travlr');*/
-
 const mongoose = require('mongoose');
 const host = process.env.DB_HOST || '127.0.0.1';
 let dbURI = 'mongodb://${host}/travlr';
@@ -113,7 +44,7 @@ process.on('SIGTERM', () => {
 
 require('./models/travlr');
 /*const path = require('path');
-console.log(path.resolve(__dirname, './travlr'));
+console.log(path.resolve(__dirname, './travlr')); // THIS WAS FOR TESTING PURPOSES WHEN I WAS HAVING A HARD TIME CONNECTING TO LOCALHOST MONGODB
 const Travlr = require('./travlr');*/
 
 
