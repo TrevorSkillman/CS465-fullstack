8f69d4769b0113bc69b0ee09d95ce99a79297724
Trevor Skillman <trevor.skillman@snhu.edu>
2023-09-28 17:22:05 -0400

    Create new application API

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,16 +1,57 @@
 
 // Importing and intializing
-const packageInfo = require('../../package.json');
+//const packageInfo = require('../../package.json');
 // fs stands for file system that allows you to access the local file system on the browser
-const fs = require('fs');
+//const fs = require('fs');
 // the pars takes the string giving back an object.
-const trips = JSON.parse(fs.readFileSync('./data/trips.json', "utf8"));
+//const trips = JSON.parse(fs.readFileSync('./data/trips.json', "utf8"));
 
-const travel = (req, res) => {
-    pageTitle = packageInfo.description + ' - Travel';
-    res.render('travel', {title: pageTitle, trips});
+const request = require('request');
+const apiOptions = {
+    server: 'http://localhost:3000'
+}
+
+const renderTravelList = (req, res, responseBody) => {
+    let message = null;
+    let pageTitle = "Travlr Webpage - Travel";
+
+    if(!(responseBody instanceof Array)){
+        message = "API lookup error";
+        responseBody = [];
+    }else{
+        if(!responseBody.length){
+            message = "No trips exist in database!";
+        }
+    }
+
+    res.render('travel', {
+        title: pageTitle,
+        trips: responseBody,
+        message
+    });
+};
+
+const travelList = (req, res) => {
+    const path = '/api/trips';
+    const requestOptions = {
+        url: `${apiOptions.server}${path}`,
+        method: "GET",
+        json: {},
+    };
+
+    console.info('>> travelController.travelList calling ' + requestOptions.url);
+
+    request(
+        requestOptions,
+        (err, { statusCode }, body) => {
+            if (err){
+                console.error(err);
+            }
+            renderTravelList(req, res, body);
+        }
+    )
 };
 
 module.exports = {
-    travel
-}
+    travelList
+};
