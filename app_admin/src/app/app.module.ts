c765f479d33499fd6757e87944c1aabf4c24e1a2
Trevor Skillman <trevor.skillman@snhu.edu>
2023-10-06 14:45:57 -0400

    251 chache files to be ignored

--- app_admin/src/app/app.module.ts
+++ app_admin/src/app/app.module.ts
@@ -13,6 +13,6 @@ import { AppComponent } from './app.component';
     HttpClientModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent] 
 })
 export class AppModule { }
